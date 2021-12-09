184f9deae4ffef60d70b370a5e6a34aeb6277332
Henry Xue <kidfullystar@gmail.com>
2021-12-10 06:34:03 +0800

    updated.

--- Fast Search.py
+++ Fast Search.py
@@ -4,14 +4,23 @@ import urllib.error
 import re
 from urllib import parse
 import socket
+import cfscrape
+from bs4 import BeautifulSoup
+import cloudscraper
 
 titles = "快来搜搜 V1.2\t   作者：Henry Xue\t  邮箱：kidfullystar@gmail.com" 
 
 def open_url(url):
-    req = urllib.request.Request(url)
-    req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36')
-    response = urllib.request.urlopen(req,timeout=20)
-    html = response.read().decode('utf-8')
+    scraper = cloudscraper.create_scraper()
+
+    resp = scraper.get(url).text
+    print(scraper.get(url).text)
+    html = BeautifulSoup(resp, "lxml")
+
+    #req = urllib.request.Request(url)
+    #req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36')
+    #response = urllib.request.urlopen(req,timeout=20)
+    #html = response.read().decode('utf-8')
     return html
 
 def get_magnet(html):
@@ -20,10 +29,10 @@ def get_magnet(html):
     size = r'<td class="size">([^<]+)'
     date = r'<td class="date">([^<]+)'
     content = ['BTC地址：13Hh3qQCKNk1M2XcS7XBJ5jAnozCpVNR3T\n','ETH & EOS地址：0x2cee6e0d082bb9daa2a82f7f5ea3150782802112\n','欢迎打赏！\n']
-    result = re.findall(p,html)
-    result_name = re.findall(name,html)
-    result_size = re.findall(size, html)
-    result_date = re.findall(date, html)
+    result = re.findall(p,str(html))
+    result_name = re.findall(name,str(html))
+    result_size = re.findall(size, str(html))
+    result_date = re.findall(date, str(html))
     for num, c in enumerate(result):
         content.append('\n%d. 资源名：%s       种子大小：%s       资源时间：%s\n下载链接：\n%s\n' % (num + 1,result_name[num],result_size[num],result_date[num],c))
     return content,result_name
