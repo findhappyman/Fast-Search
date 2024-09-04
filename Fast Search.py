7267de37bda20ffa079408fec607146fb1a13314
Henry Xue <kidfullystar@gmail.com>
2024-09-04 21:36:18 +0800

    improve this code

--- Fast Search.py
+++ Fast Search.py
@@ -1,49 +1,72 @@
-import easygui as g
-import urllib.request
-import urllib.error
+import tkinter as tk
+from tkinter import ttk, scrolledtext, messagebox
 import re
 from urllib import parse
-import socket
-from bs4 import BeautifulSoup
 import cloudscraper
-import lxml
+from bs4 import BeautifulSoup
 
-titles = "快来搜搜 V1.2\t   作者：Henry Xue\t  邮箱：kidfullystar@gmail.com" 
+TITLE = "快来搜搜 V2.0   作者：Henry Xue   邮箱：kidfullystar@gmail.com"
+SEARCH_URL = "https://torkitty.com/search/{}//"
 
 def open_url(url):
     scraper = cloudscraper.create_scraper()
-    resp = scraper.get(url).text
-    html = BeautifulSoup(resp, "lxml")
-    return html
+    resp = scraper.get(url)
+    return BeautifulSoup(resp.text, "lxml")
 
 def get_magnet(html):
-    p = r'<a href="(magnet:\?xt=urn:btih:[^"]+)'
-    name = r'<td class="name">([^<]+)'
-    size = r'<td class="size">([^<]+)'
-    date = r'<td class="date">([^<]+)'
-    content = ['ETH 地址：0x00011D3a9091F8e317f1ff3d5DcEEf5dEf77a661\n','欢迎打赏！\n']
-    result = re.findall(p,str(html))
-    result_name = re.findall(name,str(html))
-    result_size = re.findall(size, str(html))
-    result_date = re.findall(date, str(html))
-    for num, c in enumerate(result):
-        content.append('\n%d. 资源名：%s       种子大小：%s       资源时间：%s\n下载链接：\n%s\n' % (num + 1,result_name[num],result_size[num],result_date[num],c))
-    return content,result_name
+    content = ['ETH 地址：0x00011D3a9091F8e317f1ff3d5DcEEf5dEf77a661\n', '欢迎打赏！\n']
+    patterns = {
+        'magnet': r'<a href="(magnet:\?xt=urn:btih:[^"]+)',
+        'name': r'<td class="name">([^<]+)',
+        'size': r'<td class="size">([^<]+)',
+        'date': r'<td class="date">([^<]+)'
+    }
+    results = {key: re.findall(pattern, str(html)) for key, pattern in patterns.items()}
+    
+    for i, magnet in enumerate(results['magnet'], 1):
+        content.append(f"\n{i}. 资源名：{results['name'][i-1]}       种子大小：{results['size'][i-1]}       资源时间：{results['date'][i-1]}\n下载链接：\n{magnet}\n")
+    
+    return content, results['name']
+
+class Application(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title(TITLE)
+        self.geometry("800x600")
+        self.create_widgets()
+
+    def create_widgets(self):
+        # 搜索框和按钮
+        frame = ttk.Frame(self)
+        frame.pack(pady=10)
+        self.search_entry = ttk.Entry(frame, width=50)
+        self.search_entry.pack(side=tk.LEFT, padx=5)
+        search_button = ttk.Button(frame, text="搜索", command=self.search)
+        search_button.pack(side=tk.LEFT)
+
+        # 结果显示区域
+        self.result_text = scrolledtext.ScrolledText(self, wrap=tk.WORD, width=80, height=30)
+        self.result_text.pack(pady=10, padx=10)
+
+    def search(self):
+        words = self.search_entry.get()
+        if not words:
+            messagebox.showwarning("警告", "请输入搜索关键词")
+            return
+
+        url = SEARCH_URL.format(parse.quote(words))
+        try:
+            content, result_names = get_magnet(open_url(url))
+            if not result_names:
+                raise ValueError("No results found")
+            self.result_text.delete(1.0, tk.END)
+            self.result_text.insert(tk.END, "".join(content))
+        except Exception as e:
+            messagebox.showerror("错误", f"您输入的关键词无法找到资源，请尝试其它关键词，谢谢！\n错误详情: {str(e)}")
+
+def main():
+    app = Application()
+    app.mainloop()
 
 if __name__ == '__main__':
-    while 1:
-        words = g.enterbox(msg='请输入关键词（多个关键词请用空格分开）： ',title="快来搜搜 V1.2\t   作者：Henry Xue ")
-        if words == None :
-            exit(1)
-        url = r'https://torkitty.com/search/' + parse.quote(words)+ '//'
-        try:
-            content,result_name = get_magnet(open_url(url))
-            if result_name[0] == 0:
-                raise IndexError
-            g.textbox(msg="您查询的关键词对应的下载链接如下（请将下载链接用Ctrl+C粘贴至下载软件进行下载，容量为种子大小，非文件大小）:", title=titles, text=content)
-        except (socket.timeout,urllib.error.URLError,IndexError):
-            g.textbox(msg='错误信息',title=titles, text='您输入的关键词无法找到资源，请尝试其它关键词，谢谢！')
-        choices = g.ccbox(msg='是否需要继续查找',choices=('是','否'),title="快来搜搜 V1.2\t   作者：Henry Xue ")
-        if choices: pass
-        else:
-            break
+    main()
